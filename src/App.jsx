a7a7d3b9a4e6d71d9429eb2f1ede0708f1b5e32c
Jn-Sajol <julkar10121@gmail.com>
2023-11-13 09:17:20 +0600

    implement logic

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,22 @@ function App() {
     return () => clearInterval(interval);
   }, []);
 
+  return (
+    <div className="text-center">
+      <h1 className="text-4xl font-bold mb-4">Coming Soon</h1>
+      <p className="text-lg mb-8">We're working hard to bring you something awesome. Stay tuned!</p>
 
+      {/* Countdown Timer */}
+      <div className="text-2xl mb-8">
+        {countdown.days}d {countdown.hours}h {countdown.minutes}m {countdown.seconds}s
+      </div>
+
+      <div className="flex justify-center">
+        <input type="email" placeholder="Enter your email" className="px-4 py-2 rounded-l-md focus:outline-none" />
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-r-md">Notify Me</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
